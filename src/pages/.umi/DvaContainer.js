fa3ecc4a414969daf08eb81beb884b5aeddffa16
Friday <mrcuong1102@gmail.com>
2018-07-10 11:35:41 +0700

    Init

---
+++ src/pages/.umi/DvaContainer.js
@@ -0,0 +1,26 @@
+import {Component} from 'react';
+import dva from 'dva';
+import createLoading from 'dva-loading';
+
+let app = dva({
+    history: window.g_history,
+    initialState: {
+        products: [
+            {name: 'dva', id: 1},
+            {name: 'antd', id: 2},
+        ]
+    }
+});
+
+window.g_app = app;
+app.use(createLoading());
+app.model(require('../../models/products.js').default);
+
+class DvaContainer extends Component {
+    render() {
+        app.router(() => this.props.children);
+        return app.start()();
+    }
+}
+
+export default DvaContainer;
